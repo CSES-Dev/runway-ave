ecb0a00870165fc5ec7d23e0e06d53f5d9643fe7
kyla <kyla123ma@gmail.com>
2026-01-24 14:38:27 -0800

    Added no-scrollbar class

--- app/about/components/ValueBarItem.tsx
+++ app/about/components/ValueBarItem.tsx
@@ -5,7 +5,7 @@ const values = [
 export default function ValueBarItem() {
     return (
         <div className="w-full bg-gray-200 py-6 mt-12">
-            <div className="flex overflow-x-auto whitespace-nowrap px-6 md:px-10 gap-8 md:justify-between items-center max-w-screen-2xl mx-auto">
+            <div className="flex overflow-x-auto whitespace-nowrap px-6 md:px-10 gap-8 md:justify-between items-center max-w-screen-2xl mx-auto no-scrollbar">
                 {values.map((value) => (
                     <span key={value} className="text-xl lg:text-2xl font-normal text-black">
                         {value}
