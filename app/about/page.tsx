b58e762a4fcb9fdde17be9ca6fcc370d22479b07
Anthony Hu <a9hu@ucsd.edu>
2026-01-25 20:26:48 -0800

    Completed Meet the Team Section

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import MeetTheTeam from "./MeetTheTeam";
+
+export default function AboutPage() {
+    return (
+        <div className="min-h-screen">
+            <MeetTheTeam />
+        </div>
+    );
+}
