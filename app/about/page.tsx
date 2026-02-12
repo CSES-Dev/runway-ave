3c5619567f10395eafa2df955ec8ca89c457c4cb
lucashlaing <mhlaing@ucsd.edu>
2026-02-11 22:40:52 -0800

    moved homepage to main page. small link fixes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,11 @@
 import AboutSection from "./sections/AboutSection";
+import MeetTheTeam from "./sections/MeetTheTeam";
 
 export default function Page() {
     return (
         <main className="min-h-screen bg-white">
             <AboutSection />
-            {/* Meet the Team section goes here */}
+            <MeetTheTeam />
         </main>
     );
 }
