4d8bb3097f384567eb4c0b3302bacd63457d7b59
kcharry <kathycharry27@gmail.com>
2026-04-12 23:54:30 -0700

    created learn more page

---
+++ app/learn-more/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { useState, useEffect } from 'react';
+
+export default function LearnMorePage(){
+    return(
+        <main className="min-h-screen bg-[#FDFCF7] text-base pt-36">
+            <div className="px-4 md:px-16 lg:px-32 py-12 space-y-12">
+                <section className="space-y-10">
+                    <h1 className="text-3xl sm:text-4xl lg:text-5xl font-serif tracking-wide">
+                        Overview of Consumer Features
+                    </h1>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                        
+                    </div>
+                </section>
+            </div>
+
+        </main>
+    )
+}
