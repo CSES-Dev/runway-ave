7f6500b7fb2b59fea32bd594d522bc706bb4f422
Ethan Nguyen-Huu <ethannghuu@gmail.com>
2026-04-14 15:40:25 -0700

    Adjust font sizes and gradient for GeneralSupport to better match wireframes

--- app/contact/components/GeneralSupport.tsx
+++ app/contact/components/GeneralSupport.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 export default function GeneralSupport() {
     return (
-        <div className="flex items-center justify-center group w-[100vw] max-w-[1152px] h-[218px] hover:h-[316px] rounded-[3rem] overflow-hidden text-white transition-all duration-1000 bg-[linear-gradient(90deg,#363A42_0%,#8994A8_100%)] hover:bg-[linear-gradient(90deg,#8CDFA8,#34543F)]">
-            <div className="flex flex-col items-center text-center text-[clamp(1.2rem,7vw,5.5rem)] group-hover:w-[479px]">
+        <div className="flex items-center justify-center group w-[100vw] max-w-[1152px] h-[218px] hover:h-[316px] rounded-[3rem] overflow-hidden text-white transition-all duration-1000 bg-[linear-gradient(90deg,#363A42_0%,#8994A8_100%)] hover:bg-[linear-gradient(90deg,#8CDFA8_25%,#34543F_100%)]">
+            <div className="flex flex-col items-center text-center text-[clamp(1.2rem,7vw,5rem)] group-hover:w-[479px]">
                 GENERAL SUPPORT
             </div>
             <div className="w-[0px] group-hover:text-[clamp(1.2rem,1.4vw,3rem)] group-hover:pl-16 opacity-0 overflow-hidden transition-all duration-1000 group-hover:w-[497px] group-hover:opacity-100">
--- app/contact/components/PressInquiries.tsx
+++ app/contact/components/PressInquiries.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function PressInquiries() {
     return (
         <div className="flex items-center justify-center group w-[100vw] max-w-[1152px] h-[218px] hover:h-[316px] rounded-[3rem] overflow-hidden text-white transition-all duration-1000 bg-[linear-gradient(90deg,#363A42_0%,#8994A8_100%)] hover:bg-[linear-gradient(90deg,#FF7958,#691F0D_100%)]">
-            <div className="flex flex-col items-center text-center text-[clamp(1.2rem,7vw,5.5rem)] group-hover:w-[479px]">
+            <div className="flex flex-col items-center text-center text-[clamp(1.2rem,7vw,5rem)] group-hover:w-[479px]">
                 PRESS INQUIRIES
             </div>
             <div className="w-[0px] text-[clamp(1.2rem,1.4vw,3rem)] group-hover:pl-16 opacity-0 overflow-hidden transition-all duration-1000 group-hover:w-[497px] group-hover:opacity-100">
